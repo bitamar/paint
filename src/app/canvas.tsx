d7e490b559e8f21649a073ca3408b9fe9cdf4187
itamar.bar-lev <itamar.bar-lev@appsflyer.com>
2023-12-24 21:13:38 +0200

    eraser

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,8 +1,9 @@
 import { useRef, useState } from "react";
 import { Layer, Line, Stage } from "react-konva";
 import Konva from "konva";
+import styles from "./canvas.module.css";
 
-export type Line = { points: number[]; tool: string };
+export type Line = { points: number[]; tool: string; width: number };
 
 export default function Canvas({
   myLines,
@@ -13,7 +14,7 @@ export default function Canvas({
   setMyLines: (lines: Line[]) => void;
   sharedLines: Line[];
 }) {
-  const [tool] = useState("pen");
+  const [tool, setTool] = useState("pen");
 
   const isDrawing = useRef(false);
 
@@ -23,7 +24,10 @@ export default function Canvas({
     const point = stage.getPointerPosition();
     if (!point) return;
     isDrawing.current = true;
-    setMyLines([...myLines, { tool, points: [point.x, point.y] }]);
+    setMyLines([
+      ...myLines,
+      { tool, points: [point.x, point.y], width: tool == "pen" ? 5 : 10 },
+    ]);
   };
 
   const mouseMove = (event: Konva.KonvaEventObject<MouseEvent>) => {
@@ -60,31 +64,49 @@ export default function Canvas({
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={mouseDown}
-      onMousemove={mouseMove}
-      onMouseup={mouseUp}
-      onMouseLeave={mouseUp}
-      style={{ cursor: "crosshair" }}
-    >
-      <Layer>
-        {[...sharedLines, ...myLines].map((line, i) => (
-          <Line
-            key={i}
-            points={line.points}
-            stroke="#df4b26"
-            strokeWidth={5}
-            tension={0.5}
-            lineCap="round"
-            lineJoin="round"
-            globalCompositeOperation={
-              line.tool === "eraser" ? "destination-out" : "source-over"
-            }
-          />
-        ))}
-      </Layer>
-    </Stage>
+    <div>
+      <div className={styles.buttons}>
+        <button
+          onClick={() => setTool("pen")}
+          className={`${styles.button} ${tool == "pen" ? styles.pressed : ""}`}
+        >
+          pen
+        </button>
+        <button
+          onClick={() => setTool("eraser")}
+          className={`${styles.button} ${
+            tool == "eraser" ? styles.pressed : ""
+          }`}
+        >
+          eraser
+        </button>
+      </div>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={mouseDown}
+        onMousemove={mouseMove}
+        onMouseup={mouseUp}
+        onMouseLeave={mouseUp}
+        style={{ cursor: "crosshair" }}
+      >
+        <Layer>
+          {[...sharedLines, ...myLines].map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={line.width}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import Board from "@/app/board";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <Board />
-      </div>
-
-      <div className={styles.center}></div>
+      <Board />
     </main>
   );
 }
